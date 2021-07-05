73852edcab5ade0a8a4ef1d7911674cd8cfd6da5
Guilherme-Del <guilherme_cavenaghi@hotmail.com>
2021-07-05 08:45:55 -0300

    fix composer
    Instalação correta composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'de95041762b699bef6cd33cdee3df1e33ba07bbc',
+        'name' => 'php/login',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php/login' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'de95041762b699bef6cd33cdee3df1e33ba07bbc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
